d8bbe45c72fed8650da50157962b8799a2262edb
Dhruv Bansal <dhruvbansal1432@gmail.com>
2026-08-06 00:09:12 +0530

    Added problem to check whether a number is +ve, -ve or zero

---
+++ L02 - Conditions(if-else)/Problem12.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+int main(){
+    /* Write a program to read a number and check whether it is positive, negative or zero. */
+    double num;
+    std::cout<<"Enter a number: ";
+    std::cin>>num;
+
+    if(num>0){
+        std::cout<<"The number entered is a positive number\n";
+    }
+    else if(num<0){
+        std::cout<<"The number entered is a negative number\n";
+    }
+    else{
+        std::cout<<"The number entered is zero(0)\n";
+    }
+
+    /* Alternate approach: */
+    // num==0 ? std::cout<<"The number entered is zero\n" : (num>0 ? std::cout<<"The number entered is positive\n" : std::cout<<"The number entered is negative\n");
+    return 0;
+}
