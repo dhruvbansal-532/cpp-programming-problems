d130f9532ee6ff9502a5c255950b32548a24a1eb
Dhruv Bansal <dhruvbansal1432@gmail.com>
2026-08-05 23:50:13 +0530

    Checked whether a number is even or odd using if/else and ternary operator

--- L02 - Conditions(if-else)/Problem11.cpp
+++ L02 - Conditions(if-else)/Problem11.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 int main(){
     /* Problem Statement: Write a program to read a number and check whether it is even or odd. */
+    int num;
+    std::cout<<"Enter a number: ";
+    std::cin>>num;
 
+     if(num%2==0){
+        std::cout<<"The number entered is even\n";
+    }
+    else{
+        std::cout<<"The number entered is odd\n";
+    }
+
+    /* Alternate method: */
+    // num%2 != 0 ? std::cout<<"The entered number is odd\n" : std::cout<<"The entered number is even\n";
     
     return 0;
 }
